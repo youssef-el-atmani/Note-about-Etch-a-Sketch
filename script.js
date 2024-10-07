345205a6c022f6fcfc3c9b417f1e52c8ebeebc9b
jguilhermepaiva <jgpp@cin.ufpe.br>
2024-10-06 21:33:28 -0300

    Update first version complete

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 let container = document.querySelector(".container");
+let btn = document.querySelector(".btn");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let divs = document.createElement("div");
-    container.appendChild(divs);
-    divs.style.height = "6.25vh";
-    divs.style.background = "lightgrey";
-    divs.style.border = "1px solid grey";
-    divs.style.width="6.25%";
-    divs.addEventListener("mouseover", () =>{
-        divs.style.background="blue";
-    })
-    
+let size;
+
+let widthSize = 100 / size;
+
+function createGrid(size) {
+
+  container.innerHTML='';
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let divs = document.createElement("div");
+      container.appendChild(divs);
+      divs.style.height = `calc(100% / ${size})`;
+      divs.style.background = "lightgrey";
+      divs.style.border = "1px solid grey";
+      divs.style.width = `calc(100% /${size})`;
+      divs.addEventListener("mouseover", () => {
+        divs.style.background = "blue";
+      });
+    }
   }
 }
 
+btn.addEventListener("click", () => {
+  size = prompt("Digite o tamanho do grid (max 100)");
+  if (size > 100) {
+    size = 100;
+    createGrid(size);
+  } else {
+    createGrid(size);
+  }
+});
